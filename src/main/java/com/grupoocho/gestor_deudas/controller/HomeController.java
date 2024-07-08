538d8023d76f8c011934317e9794a45f354502b4
ramdhun20 <76880583+ramdhun20@users.noreply.github.com>
2024-07-08 04:07:21 -0500

    Add files via upload

---
+++ src/main/java/com/grupoocho/gestor_deudas/controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.grupoocho.gestor_deudas.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String redirectToLogin() {
+        return "redirect:/html/app.html";
+    }
+}
